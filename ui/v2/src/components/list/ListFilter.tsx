27bf02d640313bbde2bca7e4b1d3449e6470b55f
Stash Dev <stashappdev@gmail.com>
2019-02-22 19:29:01 -0800

    stuff

--- ui/v2/src/components/list/ListFilter.tsx
+++ ui/v2/src/components/list/ListFilter.tsx
@@ -16,10 +16,12 @@ import {
   Menu,
   MenuItem,
   Divider,
+  Tag,
+  ITagProps,
 } from "@blueprintjs/core";
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import { debounce } from 'lodash';
-import { ListFilter as Filter, Criteria } from '../../models/list';
+import { ListFilter as Filter, Criteria, CriteriaType } from '../../models/list';
 import { AddFilter } from './AddFilter';
 
 type ListFilterProps = {
@@ -81,40 +83,54 @@ export class ListFilter extends React.PureComponent<ListFilterProps, ListFilterS
   private onAddCriteria(criteria: Criteria) {
     this.props.filter.criterions.push(criteria);
     this.props.onChange(this.props.filter);
+    this.forceUpdate();
+  }
+
+  private onRemoveTag(e: React.MouseEvent<HTMLButtonElement>, tagProps: ITagProps) {
+    console.log(`remove ${tagProps.itemID}`)
   }
 
   public render() {
     return (
-      <div style={{display: 'flex', justifyContent: 'center', margin: '10px auto'}}>
-        <InputGroup
-          large={true}
-          placeholder="Search..."
-          onChange={this.onChangeQuery.bind(this)}
-          className="filter-item" />
-        <HTMLSelect
-          large={true}
-          style={{flexBasis: 'min-content'}}
-          options={PAGE_SIZE_OPTIONS}
-          onChange={this.onChangePageSize.bind(this)}
-          className="filter-item" />
-        <ControlGroup
-          className="filter-item">
-          <AnchorButton
-            rightIcon={ this.props.filter.sortDirection === 'asc' ? 'caret-up' : 'caret-down' }
-            onClick={this.onChangeSortDirection.bind(this)}>
-              { this.props.filter.sortDirection === 'asc' ? 'Ascending' : 'Descending' }
-          </AnchorButton>
-          <Popover position="bottom">
-            <Button large={true}>{this.props.filter.sortBy}</Button>
-            <Menu>
-              {this.props.filter.sortByOptions.map(option => (
-                <MenuItem onClick={this.onChangeSortBy.bind(this)} text={option} key={option} />
-              ))}
-            </Menu>
-          </Popover>
-        </ControlGroup>
+      <div>
+        <div style={{display: 'flex', justifyContent: 'center', margin: '10px auto'}}>
+          <InputGroup
+            large={true}
+            placeholder="Search..."
+            onChange={this.onChangeQuery.bind(this)}
+            className="filter-item" />
+          <HTMLSelect
+            large={true}
+            style={{flexBasis: 'min-content'}}
+            options={PAGE_SIZE_OPTIONS}
+            onChange={this.onChangePageSize.bind(this)}
+            className="filter-item" />
+          <ControlGroup
+            className="filter-item">
+            <AnchorButton
+              rightIcon={ this.props.filter.sortDirection === 'asc' ? 'caret-up' : 'caret-down' }
+              onClick={this.onChangeSortDirection.bind(this)}>
+                { this.props.filter.sortDirection === 'asc' ? 'Ascending' : 'Descending' }
+            </AnchorButton>
+            <Popover position="bottom">
+              <Button large={true}>{this.props.filter.sortBy}</Button>
+              <Menu>
+                {this.props.filter.sortByOptions.map(option => (
+                  <MenuItem onClick={this.onChangeSortBy.bind(this)} text={option} key={option} />
+                ))}
+              </Menu>
+            </Popover>
+          </ControlGroup>
 
-        <AddFilter filter={this.props.filter} onAddCriteria={this.onAddCriteria.bind(this)} />
+          <AddFilter filter={this.props.filter} onAddCriteria={this.onAddCriteria.bind(this)} />
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center', margin: '10px auto'}}>
+          {this.props.filter.criterions.map(criteria => (
+            <Tag className="filter-item" itemID={criteria.type.toString()} onRemove={this.onRemoveTag.bind(this)}>
+              {(CriteriaType as any)[criteria.type]} with value {criteria.value}
+            </Tag>
+          ))}
+        </div>
       </div>
     );
   }
